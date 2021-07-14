bd8939d5f2d93b862b2c0ecaa333951046e6138a
Josh Mabry <jmabry@phase2technology>
2021-07-13 17:26:34 -0700

    add utils file

---
+++ src/components/utils.ts
@@ -0,0 +1,47 @@
+import basketball from './assets/basketball.svg';
+import baseball from './assets/baseball.svg';
+import boating from './assets/boating-1.svg';
+import dogPark from './assets/dog.svg';
+import picnic from './assets/picnic-table.svg';
+import playground from './assets/playground.svg';
+import restrooms from './assets/restrooms.svg';
+import skatePark from './assets/skateboarder.svg';
+import swimming from './assets/swim.svg';
+import tennis from './assets/tennis.svg';
+
+export const allAmenities = [
+  'Basketball',
+  'Baseball',
+  'Boating',
+  'Dog Park',
+  'Picnic',
+  'Playground',
+  'Restrooms',
+  'Skateboarding',
+  'Swimming',
+  'Tennis',
+];
+
+export const parkAmenities = [
+  ['Baseball', 'Picnic', 'Playground', 'Restrooms', 'Tennis'],
+  ['Dog Park', 'Playground', 'Restrooms', 'Skateboarding'],
+  ['Dog Park', 'Basketball', 'Restrooms'],
+  ['Playground', 'Restrooms'],
+  ['Skateboarding'],
+  ['Basketball', 'Dog Park', 'Picnic'],
+  ['Dog Park', 'Picnic', 'Playground', 'Restrooms'],
+  ['Baseball', 'Restrooms', 'Tennis', 'Skateboarding'],
+];
+
+export const amenityIcons = {
+  Basketball: basketball,
+  Baseball: baseball,
+  Boating: boating,
+  'Dog Park': dogPark,
+  Picnic: picnic,
+  Playground: playground,
+  Restrooms: restrooms,
+  Skateboarding: skatePark,
+  Swimming: swimming,
+  Tennis: tennis,
+};
