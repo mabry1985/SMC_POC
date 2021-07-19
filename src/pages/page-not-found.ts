0d25430fd7360863715a34f5d660e43fc54d6fa2
Josh Mabry <jmabry@phase2technology>
2021-07-19 11:47:12 -0700

    remove references to deprecated dependencies

--- src/pages/page-not-found.ts
+++ src/pages/page-not-found.ts
@@ -1,26 +1,15 @@
 import { html, LitElement } from 'lit';
 import { customElement } from 'lit/decorators.js';
 import { urlForName } from '../router.js';
-import '../components/page-title';
-import { tailwindStyles } from '../styles';
 
 @customElement('page-not-found')
 export class PageNotFound extends LitElement {
-  static styles = [tailwindStyles];
-  // static styles = css`
-  //   :host {
-  //     display: block;
-  //   }
-  //   section {
-  //     padding: 1rem;
-  //     text-align: center;
-  //   }
-  // `;
+  static styles = [];
 
   render() {
     return html`
       <section>
-        <page-title>Page not found, Fool!</page-title>
+        <h1>Page not found, Fool!</h1>
         <p>
           <a href="${urlForName('home')}">Back to home</a>
         </p>
